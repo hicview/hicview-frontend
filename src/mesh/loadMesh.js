b100f29a2825e5fca4088aa8f6e2ff18de9002f1
HongpengM <mahongpengmars@163.com>
2019-04-14 01:27:57 +0800

    update: rm redundent files

--- src/mesh/loadMesh.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-//import * as THREE from 'three';
-function createMesh(geometry, material, callback){
-  let mesh = new THREE.Mesh(geometry, material);
-  callback(mesh);
-  return mesh;
-}
-module.exports = { 'addMesh': createMesh};
